b7a559a796478f0a552011c173e42f4cf164efda
arturobl00 <arturobl00@msn.com>
2021-08-30 12:57:38 -0500

    Introduccion

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function miApp(){ //Función principal
+  return( //Retorno
+    <h1>Hola a todos</h1> //Contenido
+  );
+}
+
+export default miApp; //export Resultado
+
